45a9e968658ec4cd46311944e632b8aa0e8598be
younmeeoh <chewybottom@gmail.com>
2020-11-24 23:08:11 -0800

    web token working correctly, adding third queryline

--- src/home/dao/trackingdao.java
+++ src/home/dao/trackingdao.java
@@ -10,13 +10,16 @@ import home.model.tracking;
 
 public class trackingdao {
 	 public int enterHours(tracking tracking) throws ClassNotFoundException {
-	        String INSERT_SHIFT_SQL = "INSERT INTO SHIFT" + 
+	        String INSERT_SHIFT_SQL = "INSERT INTO SHIFT " + 
 	        		"(Start,End,PID) VALUES"+
 	        		"(?,?,?);";
-	        String INSERT_HOURS_SQL = "INSERT INTO Hours" + 
-	        		"(UID,hWorked) VALUES" +
+	        String INSERT_HOURS_SQL = "INSERT INTO Hours " + 
+	        		"(UserID,hWorked) VALUES" +
 	        		"(SELECT ID FROM USERS WHERE username = ? ,"
-	        		+ "SELECT TIMEDIFF(end,start) FROM SHIFT where id = (SELECT ID FROM Users WHERE Username =? );";
+	        		+ "SELECT TIMEDIFF(?,?);";
+	        String INSERT_WORKINGON_SQL = "INSERT INTO Workingon " +
+	        		"(teamID,ProjectID,UserID) VALUES"+
+	        		"(?,?,?);";
 	        int result = 0;
 
 	        Class.forName("com.mysql.jdbc.Driver");
@@ -35,11 +38,23 @@ public class trackingdao {
 	            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_HOURS_SQL)) {      
 	            
 	            	//grab username via html variable passing
-	            	preparedStatement.setString(1, tracking.getTeam());
-	            	preparedStatement.setString(2, tracking.getUsername());
+	            	
+	            	preparedStatement.setString(1, tracking.getUsername());
+	            	preparedStatement.setString(2, tracking.getEnd());
+		            preparedStatement.setString(3, tracking.getStart());
 		            //make sure the same UID gets input into hWorked query.
 		            System.out.println(preparedStatement);
 		            // Step 3: Execute the query or update query
+	            	result = preparedStatement.executeUpdate();
+	            }
+	            ///insert into WORKING ON table
+	            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_WORKINGON_SQL)) {      
+		            
+	            	//grab username via html variable passing
+	            	preparedStatement.setString(1, tracking.getTeam());
+	            	preparedStatement.setString(2, tracking.getPID());
+	            	preparedStatement.setString(3, tracking.getUsername());
+		            //make sure the same UID gets input into hWorked query.
 		            System.out.println(preparedStatement);
 	            	result = preparedStatement.executeUpdate();
 	            }
@@ -48,7 +63,7 @@ public class trackingdao {
 	            // process sql exception
 	            printSQLException(e);
 	        }
-	        System.out.println("enterHour form working?");
+	  
 	        return result;
 	    }
 
